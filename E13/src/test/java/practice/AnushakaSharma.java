e688ed012ba9612afaaa3503e5f11f07944cbdb9
Dell <Dell@DESKTOP-5I4RHGI>
2025-07-29 16:59:12 +0530

    Anushka push

---
+++ E13/src/test/java/practice/AnushakaSharma.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class AnushakaSharma {
+
+	public static void main(String[] args)
+	{
+		System.out.println("my name is anushkha");
+		System.out.println("my name is anushkha");
+		System.out.println("my name is anushkha");
+		System.out.println("my name is anushkha");
+
+	}
+
+}
